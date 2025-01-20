64528b058980429627a37cdd61d7c57784e02ccd
asad ali <l1f21bsse0180@ucp.edu.pk>
2025-01-20 09:02:54 +0500

    initial commit

---
+++ configuration.sh
@@ -0,0 +1,8 @@
+echo "Repo Intial Comfiguration Started"
+git branch -m "develop"
+git config --local user.name "asad ali"
+git config --local user.email l1f21bsse0180@ucp.edu.pk
+git config --local core.editor notepad
+mkdir ./src ./styles
+touch ./src/index.html ./styles/index.css .gitignore
+npm init -y
